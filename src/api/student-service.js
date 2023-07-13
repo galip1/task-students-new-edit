84b1f5bfb176c1ec9beec416a337d32ff3508dd6
galip <galipts1@gmail.com>
2023-07-13 21:38:14 +0300

    scss

--- src/api/student-service.js
+++ src/api/student-service.js
@@ -8,10 +8,17 @@ export const getStudents = () => {
   return axios.get(`${API_URL}/users`);
 };
 
+///new student
 export const newStudent = ({ newStudentValues }) => {
   return axios.post(`${API_URL}/users/add`, newStudentValues);
 };
 
+//delete
 export const deleteStudent = (id) => {
   return axios.delete(`${API_URL}/users/${id}`);
+};
+
+///update
+export const updateStudent = (id, updateSt) => {
+  return axios.put(`${API_URL}/users/${id}`, updateSt);
 };
