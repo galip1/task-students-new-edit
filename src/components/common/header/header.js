0d174c84c9e7446e44c6397813a87ce4c77e580b
galip <galipts1@gmail.com>
2023-07-20 22:42:49 +0300

    add

---
+++ src/components/common/header/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./header.scss";
+import { TbCircleCaretLeft } from "react-icons/tb";
+import { GoBell } from "react-icons/go";
+import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <Container fluid className="header mt-4">
+      <span onClick={() => navigate("/dashboard")}>
+        <TbCircleCaretLeft />
+      </span>
+      <span>
+        <GoBell />
+      </span>
+    </Container>
+  );
+};
+
+export default Header;
