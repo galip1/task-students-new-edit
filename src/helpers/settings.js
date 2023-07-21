cb76add2e3b8e85954bd7ae7aa0df24aff85b9d2
galip <galipts1@gmail.com>
2023-07-21 10:12:18 +0300

    vercel

--- src/helpers/settings.js
+++ src/helpers/settings.js
@@ -1,6 +1,6 @@
 export const settings = {
   siteName: "sikayetvar - Task",
-  siteUrl: "victor-sikayetvar-task.vercel.app/",
+  siteUrl: "https://task-students.vercel.app/",
   email: "glpvctor@gmail.com",
   phone: "(90)-501-361-63-60",
   apiURL: "https://dummyjson.com",
